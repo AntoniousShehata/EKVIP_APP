c061739c5e669a0c78c2e7bd600b9431254a0926
Antonious Shehata <antoniousmaher@gmail.com>
2025-04-07 19:11:54 +0200

    adding pdf of the readme file

--- CommandApp.Tests/obj/Debug/net10.0/CommandApp.Tests.AssemblyInfo.cs
+++ CommandApp.Tests/obj/Debug/net10.0/CommandApp.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CommandApp.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15acee96943294888bd2bd839a02dd2e39390dc4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab60a9cfdcbe6808b7b3bea94f3085fcb5813de1")]
 [assembly: System.Reflection.AssemblyProductAttribute("CommandApp.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CommandApp.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
